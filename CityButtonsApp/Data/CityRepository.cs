8bdffeebf481a1bdd6fce5aece05519781f0dc41
TalhaBodla509 <zaidbodla786@gmail.com>
2025-01-11 17:33:50 +0500

    Add files via upload

---
+++ CityButtonsApp/Data/CityRepository.cs
@@ -0,0 +1,15 @@
+﻿// CityRepository.cs
+namespace CityButtonsApp.Data
+{
+    public static class CityRepository
+    {
+        public static List<string> Cities = new List<string>
+        {
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston",
+            "Phoenix"
+        };
+    }
+}
---
+++ CityButtonsApp/Data/ServerRepsoitory.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CityButtonsApp.Data
+{
+    public static class ServerRepository
+    {
+        public static List<string> Servers = new List<string>
+        {
+            "Server 1",
+            "Server 2",
+            "Server 3",
+            "Server 4"
+        };
+    }
+}
